bdb1451313ad522060c91aa96bfd4cf6c4088929
Балобин Дмитрий Юрьевич <DYBalobin@sberbank.ru>
2022-07-18 15:29:59 +0300

    feat: add version to file

--- hors/__main__.py
+++ hors/__main__.py
@@ -6,4 +6,4 @@ if __name__ == '__main__':
     import cloudpickle
 
     cloudpickle.register_pickle_by_value(hors)
-    cloudpickle.dump(process_phrase, open('process_phrase.pkl', 'wb'))
+    cloudpickle.dump(process_phrase, open(f'process_phrase_v{hors.VERSION}.pkl', 'wb'))
